e756490631ed7184d964b316158b56e041405958
Przemek <przemek.bzowka@gmail.com>
2021-12-03 01:12:10 +0100

    delete unnecessary code

--- src/components/layout/JobOfferFilterDrawer/JobOfferFilterDrawer.js
+++ src/components/layout/JobOfferFilterDrawer/JobOfferFilterDrawer.js
@@ -7,18 +7,7 @@ import Grid from "@mui/material/Grid";
 import { ProgrammingLanguageAvatar } from "./ProgrammingLanguageAvatar";
 import { SelectInput } from "./SelectInput";
 import { setSearchParamsToObj } from "../../../utils/setSearchParamsToObj";
-import javascript from "../../../assets/icons/javascript.png";
-import python from "../../../assets/icons/python.png";
-import java from "../../../assets/icons/java.png";
-import dotnet from "../../../assets/icons/dotnet.png";
-import php from "../../../assets/icons/php.png";
-import ruby from "../../../assets/icons/ruby.png";
-import cPlusPlus from "../../../assets/icons/c++.png";
-import golang from "../../../assets/icons/golang.png";
-import android from "../../../assets/icons/android.png";
-import ios from "../../../assets/icons/ios.png";
-import devops from "../../../assets/icons/devops.png";
-import other from "../../../assets/icons/other.png";
+import { programmingLanguages } from "../../../data/programmingLanguages";
 
 const sortOptions = [
   {
@@ -55,57 +44,6 @@ const experienceOptions = [
   {
     value: "senior",
     label: "senior",
-  },
-];
-
-const programmingLanguages = [
-  {
-    value: "javascript",
-    src: javascript,
-  },
-  {
-    value: "python",
-    src: python,
-  },
-  {
-    value: "java",
-    src: java,
-  },
-  {
-    value: ".net",
-    src: dotnet,
-  },
-  {
-    value: "php",
-    src: php,
-  },
-  {
-    value: "ruby",
-    src: ruby,
-  },
-  {
-    value: "c++",
-    src: cPlusPlus,
-  },
-  {
-    value: "golang",
-    src: golang,
-  },
-  {
-    value: "android",
-    src: android,
-  },
-  {
-    value: "ios",
-    src: ios,
-  },
-  {
-    value: "devops",
-    src: devops,
-  },
-  {
-    value: "inne",
-    src: other,
   },
 ];
 
